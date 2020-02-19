047841de3a8ead0a4b978c56f703d2fc1203e7df
fuzumin@outlook.com <fuzumin@outlook.com>
2020-02-19 21:45:35 +0800

    歌曲名字动画,细节待优化

--- my-app/src/pages/plays/cmp/head-cmp/index.tsx
+++ my-app/src/pages/plays/cmp/head-cmp/index.tsx
@@ -1,12 +1,74 @@
-import React from 'react';
-import { NoticeBar, WhiteSpace, Icon } from 'antd-mobile';
+import React,{ useState, useEffect, useRef } from 'react';
 
 import styles from './index.module.css';
 
-import leftSvg from 'assets/footer/head/left.svg'; // 返回
+import leftSvg from 'assets/footer/head/leftSvg.svg'; // 返回
 import fxSvg from 'assets/footer/head/fx30.svg'; // 分享
 
 const Index: React.FC = () => {
+  const [timeInd, setTimeInd] = useState();
+  const [ind, setInd] = useState<number>(0);
+
+  const pRef = useRef<HTMLParagraphElement | null>(null);
+  const txtRef = useRef<HTMLDivElement | null>(null);
+
+  /**
+   * 歌曲名字动画设置
+   */
+  useEffect(() => {
+    ( async () => {
+      if(pRef.current && txtRef.current) {
+        const pwidth = await ayasnGetPWidthFun(pRef.current);
+        const divwidth = await ayasnGetPWidthFun(txtRef.current);
+
+        const pwidthNum = Number.parseInt(pwidth as string);
+        const divwidthNum = Number.parseInt(divwidth as string);
+
+        if(pwidthNum > divwidthNum) {
+          const widths = Math.floor(pwidthNum - divwidthNum);
+          pRef.current.animate([
+            { transform: `translateX(0px)` },
+            { transform: `translateX(-${widths + 50}px)` },
+          ], {
+            duration: Math.floor((widths + 50) / 50) * 2000,
+            iterations: Infinity,
+            easing: 'ease'
+          });
+         
+        }
+      }
+    })();
+  }, []);
+
+  const getPWidthFun = (ele:HTMLElement) => {
+    const promise = new Promise((res, rej) => {
+      clearTimeout(timeInd)
+      const timeIndex = setTimeout(() => {
+        setTimeInd(timeIndex);
+        const eleWidth = window.getComputedStyle(ele, null).width;
+        res(eleWidth);
+      }, 50);
+    })
+    return promise.then(ele => {
+      return ele
+    })
+  };
+
+  /**
+   * 获取元素真实宽度
+   * @param ele 
+   */
+  const ayasnGetPWidthFun = async (ele:HTMLElement) => {
+    const pwidth = await getPWidthFun(ele);
+    console.log(pwidth)
+    if((pwidth === 'auto') && ( ind <=5 )) {
+      setInd(ind + 1);
+      ayasnGetPWidthFun(ele);
+    } else {
+      return pwidth;
+    }
+  };
+
 
   return (
     <div className={ styles.warpp }>
@@ -14,8 +76,8 @@ const Index: React.FC = () => {
         <div className={ styles.imgs }>
           <img src={ leftSvg } alt="返回"/>
         </div>
-        <div className={ styles.txt }>
-          <p>士大夫士大夫士大夫sdfdsf 士大夫士大夫是否s是</p>
+        <div className={ styles.txt } ref={ txtRef }>
+          <p ref={ pRef }>大sf士大夫顺sdf士大夫撒旦发射点方式发丰速递第三方速度fs f 夫11111111</p>
           <p>演唱者</p>
         </div>
       </div>
