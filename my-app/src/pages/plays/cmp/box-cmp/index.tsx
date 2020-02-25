1a485b35230431d0a0b68c0493feb7cdd5ad213d
fuzumin@outlook.com <fuzumin@outlook.com>
2020-02-25 21:29:18 +0800

    播放页面cd页面设置

--- my-app/src/pages/plays/cmp/box-cmp/index.tsx
+++ my-app/src/pages/plays/cmp/box-cmp/index.tsx
@@ -2,11 +2,22 @@ import React from 'react';
 
 import styles from './index.module.css';
 
+import cdPng from 'assets/footer/cd/cd.png';
+import swithSvg from 'assets/footer/cd/swith.png';
+
 const Index: React.FC = () => {
 
   return (
     <div className={ styles.warpp }>
-      歌词
+      <div className={ styles.swith }>
+        <div className={ styles['switch_sets'] }>
+          <img src={ swithSvg } alt="指针"/>
+        </div>
+      </div>
+      <div className={ styles.cd }>
+        <img src={ cdPng } alt="CD"/>
+        <div className={ styles.imgs }></div>
+      </div>
     </div>
   )
 }
